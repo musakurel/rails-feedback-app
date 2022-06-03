56ebd9b5398f9b766ac605ec3a138102096b5886
Musa <musa.kurel@gusto.com>
2022-06-03 14:20:30 +0300

    inbox validations

--- app/models/inbox.rb
+++ app/models/inbox.rb
@@ -1,2 +1,7 @@
 class Inbox < ApplicationRecord
+    MIN_LENGTH = 3
+    MAX_LENGTH = 50
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :name, length: {in: MIN_LENGTH..MAX_LENGTH}
 end
